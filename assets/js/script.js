c683696f43d5b93b4626f388160ee06302156b66
Ahsanwd <156592922+Ahsanwd@users.noreply.github.com>
2025-03-02 17:29:06 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Machine Learning", "Backend Development", "Python Development", "Android Development", "Web Development"],
+    strings: ["WordPress Developer", "Custom Theme Developer", "Plugin Developer", "Elementor Specialist", "Woocommerce Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
