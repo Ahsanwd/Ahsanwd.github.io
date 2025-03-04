65e63d5b4e1d52a15c26bd1212f159145e1e030f
Ahsanwd <156592922+Ahsanwd@users.noreply.github.com>
2025-03-04 17:37:03 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
     $("#contact-form").submit(function (event) {
         emailjs.init("user_EkJctoEf_MvTRvoP4");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_bb7jtpe', 'template_zxh55th', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
